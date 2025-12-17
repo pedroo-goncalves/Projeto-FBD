fc225319c56482b062b212813a52f4f8bf1ef31d
Bernardo Fernandes dos Santos <bernardof.santos@ua.pt>
2025-12-17 14:04:39 +0000

    changed driver from 17 to 18

--- persistence/session.py
+++ persistence/session.py
@@ -9,11 +9,12 @@ def get_db_connection():
     Cria conexão ao SQL Server.
     """
     conn_str = (
-        f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+        f'DRIVER={{ODBC Driver 18 for SQL Server}};'
         f'SERVER={os.getenv("SERVER")};'
         f'DATABASE={os.getenv("DATABASE")};'
         f'UID={os.getenv("UID")};'
         f'PWD={os.getenv("PWD")};'
+        f'TrustServerCertificate=yes;'
     )
     return pyodbc.connect(conn_str)
 
@@ -23,7 +24,7 @@ def test_connection():
         print("Conexão realizada com sucesso.")
         return True
     except Exception as e:
-        print("Erro na conexão.")
+        print(f"Erro na conexão: {e}")
         return False
     
 if __name__ == "__main__":
