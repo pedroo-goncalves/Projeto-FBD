6ca78630388f3debdd0490e9cf7554072699d960
Pedro Gonçalves <pmmg2006@gmail.com>
2025-12-17 18:02:12 +0000

    htmls para o projeto, ainda faltam alguns mas precisamos de trabalhar mais o restante antes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # **SGA** - Centro de Psicologia
-* **Pedro Gonçalves** (NMec: 111111) - xxx@ua.pt
+* **Pedro Gonçalves** (NMec: 126463) - [pedrommgoncalves@ua.pt](mailto:pedrommgoncalves@ua.pt)
 * **Bernardo Santos** (NMec: 125962) - [bernardof.santos@ua.pt](mailto:bernardof.santos@ua.pt)
 
 **Fundamentos de Base de Dados- Trabalho Final | 2025/2026**
--- app.py
+++ app.py
@@ -8,10 +8,34 @@ from persistence.session import test_connection
 
 app = Flask(__name__)
 
+# Rota para a página inicial
 @app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
+@app.route('/pacientes.html')
+def pacientes():
+    return render_template('pacientes.html')
+
+@app.route('/equipa.html')
+def equipa():
+    return render_template('equipa.html')
+
+# Rota para a Agenda
+@app.route('/agenda.html')
+def agenda():
+    return render_template('agenda.html')
+
+# Rota para os Relatórios (A que estava a dar erro)
+@app.route('/relatorios.html')
+def relatorios():
+    return render_template('relatorios.html')
+
+# Rota para o Login
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
-    if test_connection():
-        app.run(debug=True)
+    app.run(debug=True)
